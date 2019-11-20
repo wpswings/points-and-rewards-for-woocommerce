6c5436d067b6eb439938e1daf8cdc900db0c21e1
mohitmishra-cedcoss <mohitmishra@cedcoss.com>
2019-11-20 16:52:30 +0530

    Plugin has been updated

--- points-rewards-for-woocommerce.php
+++ points-rewards-for-woocommerce.php
@@ -13,11 +13,11 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Points and Rewards for WooCommerce
- * Plugin URI:        https://wordpress.org/plugins/points-rewards-for-woocommerce/
  * Description:       This woocommerce extension allow merchants to reward their customers with loyalty points.
  * Version:           1.0.0
  * Author:            MakeWebBetter
  * Author URI:        https://makewebbetter.com/
+ * Plugin URI:        https://wordpress.org/plugins/points-rewards-for-woocommerce/
  * Text Domain:       points-rewards-for-woocommerce
  * Domain Path:       /languages
  *
