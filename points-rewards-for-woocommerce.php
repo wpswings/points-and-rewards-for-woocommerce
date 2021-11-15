f990e0564b962025dbd8a6c69bdea43a99ae3308
aakash-cedcoss <akashtiwari@makewebbetter.com>
2021-11-15 12:28:53 +0530

    updated_par

--- points-rewards-for-woocommerce.php
+++ points-rewards-for-woocommerce.php
@@ -24,7 +24,7 @@
  * Requires at least: 5.0.0
  * Tested up to:     5.8.2
  * WC requires at least: 4.0
- * WC tested up to:  5.8.2
+ * WC tested up to:  5.9.0
  *
  * License:           GNU General Public License v3.0
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
