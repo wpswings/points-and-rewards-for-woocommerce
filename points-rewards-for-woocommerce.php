10a81548c0fade296cb4ec9cc371ac08095c3fb3
navneet-cedcoss <navneetsingh@makewebbetter.com>
2022-10-19 15:57:28 +0530

    export-issues-fix

--- points-rewards-for-woocommerce.php
+++ points-rewards-for-woocommerce.php
@@ -21,10 +21,10 @@
  * Text Domain:       points-and-rewards-for-woocommerce
  * Domain Path:       /languages
  *
- * Requires at least: 5.1.0
- * Tested up to:     6.0.2
- * WC requires at least: 5.1.0
- * WC tested up to:  6.9.4
+ * Requires at least    : 5.1.0
+ * Tested up to         : 6.0.2
+ * WC requires at least : 5.1.0
+ * WC tested up to      : 7.0.0
  *
  * License:           GNU General Public License v3.0
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
