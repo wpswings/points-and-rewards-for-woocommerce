cc1d2684a0aebe61fa7834db7e30e8b8be242d03
navneet-cedcoss <navneetsingh@makewebbetter.com>
2023-05-17 10:32:06 +0530

    adding-api

--- public/partials/wps-wpr-points-log-template.php
+++ public/partials/wps-wpr-points-log-template.php
@@ -555,6 +555,36 @@ if ( isset( $user_id ) && null != $user_id && is_numeric( $user_id ) ) {
 			</div>
 			<?php
 		}
+		if ( array_key_exists( 'api_membership_logs', $point_log ) ) {
+			?>
+			<div class="wps_wpr_slide_toggle">
+				<p class="wps_wpr_view_log_notice wps_wpr_common_slider"><?php esc_html_e( 'Membership updated via API', 'points-and-rewards-for-woocommerce' ); ?><a class ="wps_wpr_open_toggle"  href="javascript:;"></a></p>
+
+				<table class="wps_wpr_common_table">
+					<thead>
+						<tr>
+							<th class="wps-wpr-view-log-Date">
+								<span class="wps_wpr_nobr"><?php echo esc_html__( 'Date', 'points-and-rewards-for-woocommerce' ); ?></span>
+							</th>
+							<th class="wps-wpr-view-log-Status">
+								<span class="wps_wpr_nobr"><?php echo esc_html__( 'Point Status', 'points-and-rewards-for-woocommerce' ); ?></span>
+							</th>
+						</tr>
+					</thead>
+					<?php
+					foreach ( $point_log['api_membership_logs'] as $key => $value ) {
+						?>
+						<tr>
+							<td><?php echo esc_html( wps_wpr_set_the_wordpress_date_format( $value['date'] ) ); ?></td>
+							<td><?php echo '-' . esc_html( $value['api_membership_logs'] ); ?></td>
+						</tr>
+						<?php
+					}
+					?>
+				</table>
+			</div>
+			<?php
+		}
 		if ( array_key_exists( 'expired_details', $point_log ) ) {
 			?>
 			<div class="wps_wpr_slide_toggle">
