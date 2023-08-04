230db02225800da1593769b03b53981ac8c75da8
navneet-cedcoss <navneetsingh@makewebbetter.com>
2023-08-04 12:07:34 +0530

    readme-updated

--- points-rewards-for-woocommerce.php
+++ points-rewards-for-woocommerce.php
@@ -22,9 +22,9 @@
  * Domain Path:       /languages
  *
  * Requires at least    : 5.5.0
- * Tested up to         : 6.2.2
+ * Tested up to         : 6.3.0
  * WC requires at least : 5.5.0
- * WC tested up to      : 7.9.0
+ * WC tested up to      : 8.0.0
  *
  * License:           GNU General Public License v3.0
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
