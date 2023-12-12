884783d2dcf35bb213cef6edadcdfdc175f38715
navneet-cedcoss <navneetsingh@makewebbetter.com>
2023-12-12 13:27:17 +0530

    language-file =-uodate

--- points-rewards-for-woocommerce.php
+++ points-rewards-for-woocommerce.php
@@ -22,9 +22,9 @@
  * Domain Path:       /languages
  *
  * Requires at least    : 5.5.0
- * Tested up to         : 6.4.1
+ * Tested up to         : 6.4.2
  * WC requires at least : 5.5.0
- * WC tested up to      : 8.3.0
+ * WC tested up to      : 8.3.1
  *
  * License:           GNU General Public License v3.0
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
