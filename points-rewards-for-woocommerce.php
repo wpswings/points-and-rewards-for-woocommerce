c79367f821ed179522bf1d12292d0d0a62ee2894
navneet-cedcoss <navneetsingh@makewebbetter.com>
2024-04-30 18:19:29 +0530

    version-update

--- points-rewards-for-woocommerce.php
+++ points-rewards-for-woocommerce.php
@@ -24,7 +24,7 @@
  * Requires at least    : 5.5.0
  * Tested up to         : 6.4.3
  * WC requires at least : 5.5.0
- * WC tested up to      : 8.7.0
+ * WC tested up to      : 8.8.3
  *
  * License:           GNU General Public License v3.0
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
