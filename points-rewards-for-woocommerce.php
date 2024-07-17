d29e27a9a6770a647f7aa493453064db668f3259
navneet-cedcoss <navneetsingh@makewebbetter.com>
2024-07-17 16:51:38 +0530

    bug-fixes

--- points-rewards-for-woocommerce.php
+++ points-rewards-for-woocommerce.php
@@ -23,7 +23,7 @@
  * Requires Plugins: woocommerce
  *
  * Requires at least    : 5.5.0
- * Tested up to         : 6.5.5
+ * Tested up to         : 6.6
  * WC requires at least : 5.5.0
  * WC tested up to      : 9.1.2
  *
