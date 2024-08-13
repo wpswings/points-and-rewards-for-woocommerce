f26c499acaa8ba58941daaa99e7f669280238ca1
navneet-cedcoss <navneetsingh@makewebbetter.com>
2024-08-13 16:09:02 +0530

    wp-wc

--- points-rewards-for-woocommerce.php
+++ points-rewards-for-woocommerce.php
@@ -23,9 +23,9 @@
  * Requires Plugins: woocommerce
  *
  * Requires at least    : 5.5.0
- * Tested up to         : 6.6
+ * Tested up to         : 6.6.1
  * WC requires at least : 5.5.0
- * WC tested up to      : 9.1.2
+ * WC tested up to      : 9.1.4
  *
  * License:           GNU General Public License v3.0
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
