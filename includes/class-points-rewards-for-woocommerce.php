f5e0d55945704d592f6f3aa9fe8f577718eeb5c6
navneet-cedcoss <navneetsingh@makewebbetter.com>
2025-11-14 11:06:07 +0530

    v2.9.4

--- includes/class-points-rewards-for-woocommerce.php
+++ includes/class-points-rewards-for-woocommerce.php
@@ -79,7 +79,7 @@ class Points_Rewards_For_Woocommerce {
 			$this->version = REWARDEEM_WOOCOMMERCE_POINTS_REWARDS_VERSION;
 		} else {
 
-			$this->version = '2.9.3';
+			$this->version = '2.9.4';
 		}
 
 		$this->plugin_name = 'points-and-rewards-for-woocommerce';
